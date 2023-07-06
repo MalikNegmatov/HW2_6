2f8f434914914d5c4a609be7f01067e8f326d269
MalikNegmatov <maliknegmatov@gmail.com>
2023-07-06 13:11:14 +0300

    Шаг 9 : Добавить в методы из шага 5 новые исключения. 1. В метод с добавлением сотрудника нужно добавить выброс исключения из шага 7 в ситуации, когда коллекция переполнена. 2. В метод с добавлением сотрудника нужно добавить выброс исключения из шага 8 в ситуации, когда добавляемый сотрудник уже имеется в коллекции. 3. В метод с удалением сотрудника нужно добавить выброс исключения из шага 6 в ситуации, когда удаляемый сотрудник не найден. 4. В метод с поиском сотрудника нужно добавить выброс исключения из шага 6 в ситуации, когда сотрудник не найден.

--- src/test/java/Spring/hw2_6/Service/EmployeeService.java
+++ src/test/java/Spring/hw2_6/Service/EmployeeService.java
@@ -1,27 +1,38 @@
 package Spring.hw2_6.Service;
 
 import Spring.hw2_6.Model.Employee;
+import Spring.hw2_6.Exceptions.EmployeeAlreadyAddedException;
+import Spring.hw2_6.Exceptions.EmployeeStorageIsFullException;
+import Spring.hw2_6.Exceptions.EmployeeNotFoundException;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeeService {
-    private List<Employee> employees = new ArrayList<>();
-    private final int MAX_COUNT_OF_PEOPLE = 12;
+    private final List<Employee> employees = new ArrayList<>();
+    private final int MAX_SIZE_EMPLOYEES_LIST = 12;
 
-    public Employee addEmployee(String firstName, String lastName) {
+    public boolean addEmployee(String firstName, String lastName) {
         Employee employee = new Employee(firstName, lastName);
-        employees.add(employee);
-        return employee;
+        if (employees.size() == MAX_SIZE_EMPLOYEES_LIST) {
+            throw new EmployeeStorageIsFullException("Список сотрудников переполнен.");
+        } else if (employees.contains(employee)) {
+            throw new EmployeeAlreadyAddedException("Сотрудник уже в списке.");
+        } else {
+            employees.add(employee);
+            return true;
+        }
     }
 
-    public Employee deleteEmployee(String firstName, String lastName) {
+    public boolean deleteEmployee(String firstName, String lastName) {
         Employee employee = new Employee(firstName, lastName);
+
         if (employees.contains(employee)) {
             employees.remove(employee);
-            return employee;
+            return true;
+        } else {
+            throw new EmployeeNotFoundException("Сотрудник не найден в списке.");
         }
-        return employee;
     }
 
     public boolean findEmployee(String firstName, String lastName) {
@@ -29,8 +40,7 @@ public class EmployeeService {
         if (employees.contains(employee)) {
             return true;
         } else {
-            return false;
+            throw new EmployeeNotFoundException("Сотрудник не найден в списке.");
         }
     }
-
 }
