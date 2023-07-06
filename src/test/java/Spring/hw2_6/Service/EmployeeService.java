2c3b5717b6eec6bcc51051e2762cb9f4a5d910fd
MalikNegmatov <maliknegmatov@gmail.com>
2023-07-06 11:59:44 +0300

    Шаг 4: Создать сервис EmployeeService, который хранит внутри поле с коллекцией сотрудников и константу хранящее максимальное возможное количество сотрудников в фирме.

---
+++ src/test/java/Spring/hw2_6/Service/EmployeeService.java
@@ -0,0 +1,11 @@
+package Spring.hw2_6.Service;
+
+import Spring.hw2_6.Model.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeService {
+    private List<Employee> employees = new ArrayList<>(List.of());
+    private final int MAX_COUNT_OF_PEOPLE = 12;
+}
