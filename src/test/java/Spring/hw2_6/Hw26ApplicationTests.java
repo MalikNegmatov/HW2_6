835fc8f5cd8cc3c86ad9c3d7b702dd6c2495bbae
MalikNegmatov <maliknegmatov@gmail.com>
2023-07-06 11:21:09 +0300

    Открыли проект

---
+++ src/test/java/Spring/hw2_6/Hw26ApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring.hw2_6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw26ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
