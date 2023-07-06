f7622125d2ff6b85eef8526fdcece98a6af3d92b
MalikNegmatov <maliknegmatov@gmail.com>
2023-07-06 12:19:02 +0300

    Шаг 5: Реализовать в сервисе три метода, которые принимают в качестве параметров firstName и lastName:
    1. Добавить сотрудника.
    2. Удалить сотрудника.
    3. Найти сотрудника.

--- src/test/java/Spring/hw2_6/Model/Employee.java
+++ src/test/java/Spring/hw2_6/Model/Employee.java
@@ -1,20 +1,24 @@
 package Spring.hw2_6.Model;
+
 public class Employee {
     private final String firstName;   // Имя
     private final String lastName;    // Фамилия
 
     /* Конструктор класса
      */
-    public Employee(String firstName, String middleName, String lastName, int department, int salary) {
+    public Employee(String firstName, String lastName) {
         this.firstName = formatField(firstName);
         this.lastName = formatField(lastName);
     }
+
     public String getFirstName() {
         return firstName;
     }
+
     public String getLastName() {
         return lastName;
     }
+
     @Override
     public String toString() {
         return String.format("%-15s%-15s", this.getFirstName(), this.getLastName());
@@ -40,6 +44,7 @@ public class Employee {
 
     /**
      * Метод форматирования вводимых значеий в поля ФИО
+     *
      * @param field - строка переданная в конструктор
      * @return возвращает в формате первая заглавная, остальные строчные
      */
--- src/test/java/Spring/hw2_6/Service/EmployeeService.java
+++ src/test/java/Spring/hw2_6/Service/EmployeeService.java
@@ -6,6 +6,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeeService {
-    private List<Employee> employees = new ArrayList<>(List.of());
+    private List<Employee> employees = new ArrayList<>();
     private final int MAX_COUNT_OF_PEOPLE = 12;
+
+    public Employee addEmployee(String firstName, String lastName) {
+        Employee employee = new Employee(firstName, lastName);
+        employees.add(employee);
+        return employee;
+    }
+
+    public Employee deleteEmployee(String firstName, String lastName) {
+        Employee employee = new Employee(firstName, lastName);
+        if (employees.contains(employee)) {
+            employees.remove(employee);
+            return employee;
+        }
+        return employee;
+    }
+
+    public boolean findEmployee(String firstName, String lastName) {
+        Employee employee = new Employee(firstName, lastName);
+        if (employees.contains(employee)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
